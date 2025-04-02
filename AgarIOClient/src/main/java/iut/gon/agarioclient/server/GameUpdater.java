bbbd5c1ea65ab1731ac0af480ea1b8daaca1200c
Cyprien <102376698+6pri2@users.noreply.github.com>
2025-04-02 12:25:40 +0200

    Debug erreur GameUpdater.java

--- AgarIOClient/src/main/java/iut/gon/agarioclient/server/GameUpdater.java
+++ AgarIOClient/src/main/java/iut/gon/agarioclient/server/GameUpdater.java
@@ -21,7 +21,7 @@ public  class GameUpdater implements Runnable {
                 // Envoyer l'objet uniquement aux clients prêts
                 for (ClientHandler client : Server.getClientHandlersSet()) {
                     Boolean isReady = Server.getClientReadyStatus().get(client.getClientId());
-                    if (isReady != null && isReady) {  // Seulement si le client a répondu "pret"
+                    /*if (isReady != null && isReady) {  // Seulement si le client a répondu "pret"
                         try {
                             //client.sendObject(vecteur);
                             Serializer.sendObject(game, client.getOut());
@@ -29,7 +29,7 @@ public  class GameUpdater implements Runnable {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                    }
+                    }*/
                 }
                 Thread.sleep(1000); // Pause d'une seconde entre chaque envoi (adapter si nécessaire)
             }
